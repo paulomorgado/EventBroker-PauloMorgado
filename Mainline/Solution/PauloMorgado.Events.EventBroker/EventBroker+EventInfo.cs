0bf4bdc6ecafd7b56294edad61c744e6f226c5c8
Paulo Morgado <paulomorgado@users.noreply.github.com>
2010-06-23 00:07:53 +0000

    Add support for exclusive publishers.

---
+++ Mainline/Solution/PauloMorgado.Events.EventBroker/EventBroker+EventInfo.cs
@@ -0,0 +1,55 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EventBroker+EventInfo.cs"
+//            project="PauloMorgado.Events.EventBroker"
+//            assembly="PauloMorgado.Events.EventBroker"
+//            solution="PMEventBroker"
+//            company="Paulo Morgado">
+//     Copyright (c) Paulo Morgado. All rights reserved.
+// </copyright>
+// <author>Paulo Morgado</author>
+// <summary>Event information.</summary>
+//-----------------------------------------------------------------------
+
+namespace PauloMorgado.Events
+{
+    using System.Collections.Generic;
+
+    /// <content>
+    /// Event information.
+    /// </content>
+    public partial class EventBroker
+    {
+        /// <summary>
+        /// Subscription information.
+        /// </summary>
+        private class EventInfo
+        {
+            /// <summary>
+            /// The publisher.
+            /// </summary>
+            public object Publisher;
+
+            /// <summary>
+            /// The list of subscriptions.
+            /// </summary>
+            private List<EventSubscriptionInfo> subscriptions;
+
+            /// <summary>
+            /// Gets the list of subscriptions.
+            /// </summary>
+            /// <value>The list of subscriptions.</value>
+            public List<EventSubscriptionInfo> Subscriptions
+            {
+                get
+                {
+                    if (this.subscriptions == null)
+                    {
+                        this.subscriptions = new List<EventSubscriptionInfo>();
+                    }
+
+                    return this.subscriptions;
+                }
+            }
+        }
+    }
+}
